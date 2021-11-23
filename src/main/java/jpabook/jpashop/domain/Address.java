c5f2fbca1ab19d2908fc953a4acaef7c046c3621
hanbee <sonhanbi1002@gmail.com>
2021-11-24 00:08:38 +0900

    값을 제공하는 Address 엔티티의 경우 setter 없이 생성자를 통해서만 값을 지정하도록 설정

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,12 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { }  // JPA 스펙 상 생성한 생성자
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
