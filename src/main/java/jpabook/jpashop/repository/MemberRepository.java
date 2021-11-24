756ef86c3623005a9c23a0fb881477455d36b4af
hanbee <sonhanbi1002@gmail.com>
2021-11-24 20:48:55 +0900

    MemberRepository 메소드 추가 생성

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -12,12 +13,42 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
+    /**
+     * member 저장
+     * @param member
+     * @return
+     */
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
     }
 
+    /**
+     * id로 member 조회
+     * @param id
+     * @return
+     */
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    /**
+     * 전체 member 조회
+     * @return
+     */
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /**
+     * name으로 member 조회
+     * @param name
+     * @return
+     */
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
